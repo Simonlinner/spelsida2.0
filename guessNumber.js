33d4ef2bbacc2e2f7d06600c721a07b60ba548dc
Simon Linnér <simon.linner@learnet.se>
2024-04-16 10:16:04 +0200

    i

---
+++ guessNumber.js
@@ -0,0 +1,19 @@
+let y = Math.floor(Math.random() * 100 + 1);
+let guess = 1;
+
+document.getElementById("submitguess").onclick = function () {
+    let x = document.getElementById("guessField").value;
+
+    if (x == y) {
+        alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+            + guess + " GUESS ");
+    }
+    else if (x > y) {
+        guess++;
+        alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+    }
+    else {
+        guess++;
+        alert("OOPS SORRY!! TRY A GREATER NUMBER")
+    }
+}
