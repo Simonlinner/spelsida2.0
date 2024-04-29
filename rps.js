389d5207b36849c13e3152b1e7822533636d4ac1
Liam Larsson <liam.larsson7@learnet.se>
2024-04-29 15:07:50 +0200

    365

--- rps.js
+++ rps.js
@@ -4,7 +4,7 @@ const game = () => {
     let moves = 0;
  
  
-    // Function to 
+
     const playGame = () => {
         const rockBtn = document.querySelector('.rock');
         const paperBtn = document.querySelector('.paper');
@@ -12,7 +12,7 @@ const game = () => {
         const playerOptions = [rockBtn, paperBtn, scissorBtn];
         const computerOptions = ['rock', 'paper', 'scissors']
  
-        // Function to start playing game
+
         playerOptions.forEach(option => {
             option.addEventListener('click', function () {
  
@@ -24,10 +24,10 @@ const game = () => {
                 const choiceNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[choiceNumber];
  
-                // Function to check who wins
+
                 winner(this.innerText, computerChoice)
- 
-                // Calling gameOver function after 10 moves
+
+
                 if (moves == 10) {
                     gameOver(playerOptions, movesLeft);
                 }
@@ -36,7 +36,7 @@ const game = () => {
  
     }
  
-    // Function to decide winner
+
     const winner = (player, computer) => {
         const result = document.querySelector('.result');
         const playerScoreBoard = document.querySelector('.p-count');
@@ -82,7 +82,7 @@ const game = () => {
         }
     }
  
-    // Function to run when game is over
+
     const gameOver = (playerOptions, movesLeft) => {
  
         const chooseMove = document.querySelector('.move');
@@ -120,10 +120,10 @@ const game = () => {
     }
  
  
-    // Calling playGame function inside game
+
     playGame();
  
 }
  
-// Calling the game function
+
 game();
