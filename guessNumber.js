192b019cfc5b648fd979ab6cf424d4d167b608c6
Liam Larsson <liam.larsson7@learnet.se>
2024-04-30 09:54:32 +0200

    458

--- guessNumber.js
+++ guessNumber.js
@@ -1,9 +1,16 @@
+// Generar ett readnom tal mellan 1 - 100 som sparas i variablen y
 let y = Math.floor(Math.random() * 100 + 1);
+
+// Variabel för att hålla reda på antal gissningar. Börjar med 1 eftersom spelaren gör sin första gissning.
 let guess = 1;
 
-document.getElementById("submitguess").onclick = function () {
+// Funktionen körs när användaren klickar på knappen med id 'submitguess'
+    document.getElementById("submitguess").onclick = function () {
+
+// Hämtar värdet med id 'guessField' och sparar det i variabeln 'x'
     let x = document.getElementById("guessField").value;
 
+// Jämför användarens gissning med det slumpmässigt genererade numret 'y'
     if (x == y) {
         alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
             + guess + " GUESS ");
--- rps.js
+++ rps.js
@@ -1,33 +1,38 @@
+// Definiera en funktion som kallas 'game' som omfattar hela spellkoden
 const game = () => {
+    // Skapar spelar- och datorscores samt räkna antalet drag
     let playerScore = 0;
     let computerScore = 0;
     let moves = 0;
  
  
-
+// Funktion för att spela spelet
     const playGame = () => {
+
+        // Knappar för sten, sax och påse
         const rockBtn = document.querySelector('.rock');
         const paperBtn = document.querySelector('.paper');
         const scissorBtn = document.querySelector('.scissor');
         const playerOptions = [rockBtn, paperBtn, scissorBtn];
         const computerOptions = ['rock', 'paper', 'scissors']
  
-
+// Loopa igenom varje omgång
         playerOptions.forEach(option => {
             option.addEventListener('click', function () {
- 
+
+// Öka antalet drag och uppdatera visning av återstående drag
                 const movesLeft = document.querySelector('.movesleft');
                 moves++;
                 movesLeft.innerText = `Moves Left: ${10 - moves}`;
  
- 
+// Generera random datorval
                 const choiceNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[choiceNumber];
  
-
+// Avgör vinnaren av rundan
                 winner(this.innerText, computerChoice)
 
-
+// Kontrollera om maximalt antal drag har nåtts för att avsluta spelet
                 if (moves == 10) {
                     gameOver(playerOptions, movesLeft);
                 }
@@ -36,13 +41,15 @@ const game = () => {
  
     }
  
-
+// Funktion för att avgöra vinnaren för varje runda
     const winner = (player, computer) => {
         const result = document.querySelector('.result');
         const playerScoreBoard = document.querySelector('.p-count');
         const computerScoreBoard = document.querySelector('.c-count');
         player = player.toLowerCase();
         computer = computer.toLowerCase();
+
+        // Jämför spelar- och datorval för att avgöra vinnaren
         if (player === computer) {
             result.textContent = 'Tie'
         }
@@ -58,6 +65,8 @@ const game = () => {
                 playerScoreBoard.textContent = playerScore;
             }
         }
+
+// alla olika alternativ för sten, sax och påse
         else if (player == 'scissors') {
             if (computer == 'rock') {
                 result.textContent = 'Computer Won';
@@ -82,9 +91,10 @@ const game = () => {
         }
     }
  
-
+// Funktion för att hantera slutet av spelet
     const gameOver = (playerOptions, movesLeft) => {
  
+// Göm spelaralternativ och visa meddelande om spelets slut
         const chooseMove = document.querySelector('.move');
         const result = document.querySelector('.result');
         const reloadBtn = document.querySelector('.reload');
@@ -97,6 +107,7 @@ const game = () => {
         chooseMove.innerText = 'Game Over!!'
         movesLeft.style.display = 'none';
  
+// Avgör spelet och visa vinst eller förlust meddelande
         if (playerScore > computerScore) {
             result.style.fontSize = '2rem';
             result.innerText = 'You Won The Game'
@@ -112,6 +123,8 @@ const game = () => {
             result.innerText = 'Tie';
             result.style.color = 'grey'
         }
+
+// Lägg till funktionalitet för omstartsknappen
         reloadBtn.innerText = 'Restart';
         reloadBtn.style.display = 'flex'
         reloadBtn.addEventListener('click', () => {
@@ -120,10 +133,10 @@ const game = () => {
     }
  
  
-
+// Starta spelet
     playGame();
  
 }
  
-
+// Kalla på game-funktionen för att starta spelet
 game();
