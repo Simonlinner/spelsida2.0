9637e9e3e65e81c602f69637b654f8e02b9c4ee3
Simon Linnér <simon.linner@learnet.se>
2024-05-07 23:11:34 +0200

    k

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@ function toggleMenu() { // Funktionen toggleMenu() används för att visa eller 
     var menu = document.querySelector('.menu');
 
     // Kontrollera om menyn är synlig eller dold
-    // Om menyn är synlig (display är 'block'), dölj den (display blir 'none')
     // Om menyn är dold (display är 'none'), visa den (display blir 'block')
+    // Block = Synlig, none = inte synlig
     menu.style.display = (menu.style.display === 'block') ? 'none' : 'block'; 
 } 
